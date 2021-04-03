d91430083ad5667387d242254fcfea4b24cb8dce
josephrt <josephrti@gmail.com>
2021-04-03 10:22:13 +0300

    Initial Commit

---
+++ kito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kito
+//
+//  Created by Егор Уваров on 03.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
